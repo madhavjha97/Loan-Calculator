96e7bb654e16c30404260ce85ac1e842688a4daa
Madhav Jha <82491455+madhavjha97@users.noreply.github.com>
2023-01-08 20:06:47 +0530

    Add files via upload

---
+++ loan.js
@@ -0,0 +1,13 @@
+function computeLoan() {
+    let amount = document.getElementById('amount').value;
+    let interest_rate = document.getElementById('interest_rate').value;
+    let months = document.getElementById('months').value;
+
+    let interest = (amount * (interest_rate * .01)) / months;
+
+    let payment = ((amount / months) + interest).toFixed(2);
+
+    //payment = payment.toString().repeat()
+
+    document.getElementById('payment').innerHTML = "Monthly Payment = ₹" +payment
+}
